d37f6823c1eb1258ad52251215401f756d73d539
Ayla <141282652+atrioshka@users.noreply.github.com>
2024-12-16 05:43:35 -0500

    Modified the main render loop
    -Replaced the original code to use the utility functions from vec3.h and color.h
    -Tested code by redirecting to a new .ppm file ("image2.ppm) and comparing it with the first .ppm file ("image.ppm) to verify that it produces the same output

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -1,4 +1,7 @@
 ﻿#include <iostream>
+#include "color.h"
+#include "vec3.h"
+
 int main() {
 
 	//Image
@@ -23,7 +26,10 @@ int main() {
 		std::clog << "r\Scanlines remaining: " << (image_height - i) << ' ' << std::flush;
 		for (int j = 0; j < image_width; j++) {
 
-			auto r = (double)(j) / (image_width - 1);
+			auto pixel_color = color(double(j) / (image_width - 1), double(i) / (image_height - 1), 0);
+			write_color(std::cout, pixel_color);
+			
+			/*auto r = (double)(j) / (image_width - 1);
 			auto g = (double(i) / (image_height - 1));
 			auto b = 0.0;
 
@@ -31,7 +37,7 @@ int main() {
 			int ig = int(255.999 * g);
 			int ib = int(255.999 * b);
 
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';*/
 
 		}
 	}
